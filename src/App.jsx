433eb1b5d396f863e7bde710e4c638f43d1bba91
miguelmanabo4 <miguelmanabo4@gmail.com>
2024-12-05 15:29:22 +0000

    Created navigation logic 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,47 @@
 import { useState, useEffect } from "react";
 import './App.css'
-import Insights from "./components/Insights";
-import List from "./components/List";
-import Schedule from "./components/Schedule";
 
 
 function App() {
   const [state, setState] = useState(0)
 
+  const handleNavigation = () => {
+    switch(state) {
+      case 0:
+        return "Insights"
+      case 1:
+        return "List"
+      case 2:
+        return "Schedule"
+      default:
+        return "Insights"
+    }
+  }
+
   return (
     <>
+      {/*NAVIGATION LOGIC 2*/}
       <button onClick={() => { setState(0) }}> INSIGHTS </button>
       <button onClick={() => { setState(1) }}> LIST </button>
       <button onClick={() => { setState(2) }}> SCHEDULE </button>
       <div>
-        { state === 0 && <Insights />}
-        { state === 1 && <List />}
-        { state === 2 && <Schedule />}
+        { handleNavigation() }
       </div>
     </>
   )
 }
 
 export default App;
+
+/*
+  NAVIGATION LOGIC 1
+
+  <button onClick={() => { setState(0) }}> INSIGHTS </button>
+  <button onClick={() => { setState(1) }}> LIST </button>
+  <button onClick={() => { setState(2) }}> SCHEDULE </button>
+  <div>
+    { state === 0 && <Insights />}
+    { state === 1 && <List />}
+    { state === 2 && <Schedule />}
+  </div>
+*/
