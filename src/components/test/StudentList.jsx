cf4147c1448cc5804b5eada9df12391dfadc5427
miguelmanabo4 <miguelmanabo4@gmail.com>
2024-12-09 13:16:25 +0000

    Filter system for eligible days

--- src/components/test/StudentList.jsx
+++ src/components/test/StudentList.jsx
@@ -4,10 +4,11 @@ import { collection, getDocs } from "firebase/firestore";
 
 function StudentList() {
     const [studentList, setStudentList] = useState([]);
+    const [courseList, setCourseList] = useState([]);
     const [filteredList, setFilteredList] = useState([]);
     const [state, setState] = useState(0);
 
-    const fetchList = async () => {
+    const fetchStudentList = async () => {
         const collectionRef = collection(db, 'studentList');
         const querySelection = await getDocs(collectionRef);
         const students = querySelection.docs.map(doc => ({
@@ -18,19 +19,31 @@ function StudentList() {
         console.log(students);
     };
 
+    const fetchClassList = async () => {
+        const collectionRef = collection(db, 'dayEligible');
+        const querySelection = await getDocs(collectionRef);
+        const courses = querySelection.docs.map(course => ({
+            id: course.id,
+            ...course.data()
+        }));
+        setCourseList(courses);
+        console.log(courses);
+    };
+
     useEffect(() => {
-        fetchList();
+        fetchStudentList();
+        fetchClassList();
     }, []);
 
     const filterList = (course) => {
         const selectedCourses = studentList.filter((student) => student.course === course);
         setFilteredList(selectedCourses);
-        setState(1); // Set state to show filtered list
+        setState(1);
     };
 
     const showAllStudents = () => {
         setFilteredList([]);
-        setState(0); // Set state to show all students
+        setState(0);
     };
 
     const StudentItem = ({ student }) => (
@@ -66,44 +79,83 @@ function StudentList() {
         );
     }
 
+    const filterByEligibility = (dayEligibleId) => {
+        // Find the course eligibility by its ID
+        const dayEligible = courseList.find((course) => course.id === dayEligibleId);
+        console.log(dayEligible);
+
+        if (!dayEligible) return;
+
+        // Get the keys of the courses that are eligible (assuming they are true)
+        const eligibleCourses = Object.keys(dayEligible).filter((course) => dayEligible[course] === true);
+        console.log(eligibleCourses);
+
+        // Filter students based on eligible courses
+        const eligibleStudents = studentList.filter((student) =>
+            eligibleCourses.includes(student.course)
+        );
+
+        setFilteredList(eligibleStudents);
+        setState(1);
+    };
+
     return (
         <>
             <div className="border-[1px] border-black flex justify-evenly">
-                <button 
+                <button
                     className="border-[1px] border-black p-[5px] w-[75px]"
-                    onClick={() => filterList('class1')}>
+                    onClick={() => filterList('class1')}
+                >
                     class 1
                 </button>
-                <button 
+                <button
                     className="border-[1px] border-black p-[5px] w-[75px]"
-                    onClick={() => filterList('class2')}>
+                    onClick={() => filterList('class2')}
+                >
                     class 2
                 </button>
-                <button 
+                <button
                     className="border-[1px] border-black p-[5px] w-[75px]"
-                    onClick={() => filterList('class3')}>
+                    onClick={() => filterList('class3')}
+                >
                     class 3
                 </button>
-                <button 
+                <button
                     className="border-[1px] border-black p-[5px] w-[75px]"
-                    onClick={() => filterList('class4')}>
+                    onClick={() => filterList('class4')}
+                >
                     class 4
                 </button>
-                <button 
+                <button
                     className="border-[1px] border-black p-[5px] w-[75px]"
-                    onClick={() => filterList('class5')}>
+                    onClick={() => filterList('class5')}
+                >
                     class 5
                 </button>
-                <button 
+                <button
                     className="border-[1px] border-black p-[5px] w-[75px]"
-                    onClick={() => filterList('class6')}>
+                    onClick={() => filterList('class6')}
+                >
                     class 6
                 </button>
-                <button 
+                <button
                     className="border-[1px] border-black p-[5px] w-[75px]"
-                    onClick={showAllStudents}>
+                    onClick={showAllStudents}
+                >
                     Show All
                 </button>
+            </div>
+
+            <div className="border-[1px] border-black flex justify-evenly mt-[10px]">
+                {courseList.map((dayEligible) => (
+                    <button
+                        key={dayEligible.id}
+                        className="border-[1px] border-black p-[5px] w-[100px]"
+                        onClick={() => filterByEligibility(dayEligible.id)}
+                    >
+                        {dayEligible.id}
+                    </button>
+                ))}
             </div>
 
             <div>
